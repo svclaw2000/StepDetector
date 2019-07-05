1223fbb22625ca0aaa945da639b2122a528ccab6
K.H.Park <svclaw2000@gmail.com>
2019-07-05 14:09:09 +0900

    RF test

--- RandomForest/RF_Train.py
+++ RandomForest/RF_Train.py
@@ -5,7 +5,10 @@ import pickle
 
 df = pd.read_csv('RedefinedData.txt', sep=' ')
 
-feature_names = ['gyro0mean', 'gyro0var', 'gyro1mean', 'gyro1var', 'gyro2mean', 'gyro2var']
+feature_names = [
+#	'acce0mean', 'acce0var', 'acce1mean', 'acce1var', 'acce2mean', 'acce2var',
+	'gyro0mean', 'gyro0var', 'gyro1mean', 'gyro1var', 'gyro2mean', 'gyro2var'
+]
 x_train, y_train = df[feature_names], df['class']
 
 rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=123456)
